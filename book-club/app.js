4b84aef26848354926829e0e6f8a805ce540e9c1
Miguel Sousa Machado <mikesmachado@gmail.com>
2022-06-03 15:09:03 +0100

    initial setup done

--- book-club/app.js
+++ book-club/app.js
@@ -10,12 +10,11 @@ require('./db')
 const express = require('express')
 
 const app = express()
-const expressLayouts = require('express-ejs-layouts')
-app.use(expressLayouts)
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require('./config')(app)
 
+// default value for title local
 const capitalized = require('./utils/capitalized')
 const projectName = 'book-club'
 
@@ -24,9 +23,6 @@ app.locals.appTitle = `${capitalized(projectName)} created with IronLauncher`
 // 👇 Start handling routes here
 const index = require('./routes/index.routes')
 app.use('/', index)
-
-const authRoutes = require('./routes/auth.routes')
-app.use('/auth', authRoutes)
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app)
